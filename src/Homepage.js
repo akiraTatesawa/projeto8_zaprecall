7c310bb0f2ab16cd1b9696064707509d3b179522
Arthur Akira <arthurak10.at@gmail.com>
2022-05-11 11:59:45 -0300

    Layout: Homepage

---
+++ src/Homepage.js
@@ -0,0 +1,11 @@
+export default function Homepage() {
+  return (
+    <section className="homepage">
+      <div className="homepage-content">
+        <img src="./assets/img/logo1.png" alt="logo" />
+        <h1>ZapRecall</h1>
+        <button>Iniciar Recall!</button>
+      </div>
+    </section>
+  );
+}
